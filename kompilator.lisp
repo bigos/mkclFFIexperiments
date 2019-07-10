7e7b7c68f35a9c57cec40c989ac5461881c9ed38
Jacek Podkanski <ruby.object@googlemail.com>
2019-07-10 23:19:51 +0100

    important readme

--- kompilator.lisp
+++ kompilator.lisp
@@ -1,8 +1,7 @@
-(defconstant +standalone-exe+ (compile-file-pathname "standalone" :type :program))
-(c::build-program +standalone-exe+
-		  :lisp-files
-		  (list (compile-file-pathname "hello.lisp" :type :object))
-		  :ld-flags
-		  (list (namestring (compile-file-pathname "hello_aux.c" :type :object)))
-		  :epilogue-code
-		  '(si::quit))
+(format t "Welcome to Man Kai~%")
+
+(in-package "COMPILER")
+
+(compile-file "hello.lisp" :system-p t)
+
+(mkcl-extensions:quit)
